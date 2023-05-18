39c8c69e4c7733e881acd2cf6b6c54d785e917e0
Maryline Zhang <maryline.zhang@hotmail.com>
2023-05-18 12:58:27 +0200

    Finiteness.v essaie

--- Finiteness.v
+++ Finiteness.v
@@ -1,5 +1,6 @@
 Require Export Similar.
 Import ListNotations.
+Require Import Lia.
 
 (** * A regexp has a finite number of canonical derivatives *)
 
@@ -57,7 +58,13 @@ Fixpoint derivs_bound r :=
 Lemma derivs_bound_ok r :
  length (over_derivs r) = derivs_bound r.
 Proof.
-Admitted.
+  induction r; simpl in *; firstorder.
+  - unfold mixcat. rewrite map_length, product_length, sublists_length. firstorder.
+  - unfold mixstar. f_equal. rewrite map_length, sublists_length. firstorder.
+  - rewrite map_length, product_length. firstorder.
+  - rewrite map_length, product_length. firstorder.
+  - rewrite map_length. firstorder.
+Qed.
 
 (** The predicate we're trying to establish on [over_derivs] *)
 
@@ -70,19 +77,57 @@ Lemma mixcat_in r r2 l1 l2 :
  exists s l2b,
    canon (Or (Cat s r2) (OR.mk l2b)) = r /\ In s l1 /\ Incl l2b l2.
 Proof.
-Admitted.
+  unfold mixcat. split; intros.
+  - apply in_map_iff in H. destruct H, x, H. exists r0, l. apply product_ok in H0. destruct H0. apply sublists_spec in H1. intuition.
+  - destruct H, H, H, H0. apply in_map_iff. exists (x,x0). intuition. apply product_ok. intuition. apply sublists_spec. assumption.
+Qed.
+
 
 Lemma mixcat_stable_1 r r2 l1 l2 : Canonical r ->
  In r l1 ->
  In (sCat r (canon r2)) (mixcat r2 l1 l2).
 Proof.
-Admitted.
+  induction r; simpl; intros; apply mixcat_in.
+  - exists Void, []. intuition.
+  - exists Epsilon, []. simpl. intuition. unfold sOr, sCat. case RE.eqb_spec; intuition.
+  - exists (Letter t), []. simpl. intuition. unfold sOr, sCat. case RE.eqb_spec; intuition.
+  - exists (Cat r1 r3), []. simpl. intuition. unfold sOr. simpl. case RE.eqb_spec.
+    + intro. rewrite <- e. f_equiv. apply can_canon_id in H4, H6. rewrite H4, H6. unfold sCat. case RE.eqb_spec; intuition. simpl. case RE.eqb_spec; intuition. case RE.eqb_spec. intro; intuition. case RE.eqb_spec. intro; intuition. intro; intuition.
+    + intro. f_equiv. apply can_canon_id in H4, H6. unfold sCat. case RE.eqb_spec. intro. intuition congruence. intro. simpl. case RE.eqb_spec. intro. intuition congruence. intro. case RE.eqb_spec. intuition congruence. intro. case RE.eqb_spec. intuition congruence. intro. intuition congruence.
+  - exists (Star r), []. simpl. intuition. unfold sOr. simpl. case RE.eqb_spec.
+    + intro. rewrite <- e. f_equiv. unfold sStar. apply can_canon_id in H3. case RE.eqb_spec. intro. intuition congruence. intro. simpl. case RE.eqb_spec. intuition congruence. intuition congruence.
+    + intro. f_equiv. apply can_canon_id in H3. unfold sStar. case RE.eqb_spec. intuition congruence. intro. simpl. case RE.eqb_spec. intuition congruence. intuition congruence.
+  - exists (Or r1 r3), []. simpl. intuition. rewrite sOr_void_r. f_equiv. rewrite <- canon_or with(r:=r1)(s:=r3). apply can_canon_id. simpl. intuition.
+  - exists (And r1 r3), []. simpl. intuition. apply can_canon_id in H2, H4. rewrite sOr_void_r. f_equiv. unfold sAnd. case RE.eqb_spec.  intuition congruence. intro. simpl. case RE.eqb_spec. intuition congruence. intuition congruence.
+  - exists (Not r), []. simpl. intuition. rewrite sOr_void_r. f_equiv. apply can_canon_id in H. intuition congruence.
+Qed.
 
 Lemma mixcat_stable_2 r r' r2 l1 l2 : Canonical r' ->
  In r (mixcat r2 l1 l2) ->
  In r' l2 ->
  In (sOr r r') (mixcat r2 l1 l2).
 Proof.
+  induction r; simpl; intros; apply mixcat_in; apply mixcat_in in H0; destruct H0, H0.
+  - exists x, [r']. intuition. unfold sOr. case RE.eqb_spec.
+    + apply can_canon_id in H. intro. simpl in *. apply sOr_void in H2.  destruct H2.
+      * rewrite H2. rewrite sOr_void_l. assumption.
+      * apply sCat_can; apply canon_can.
+      * apply canon_can.
+    + intuition.
+    + apply Incl_singleton. assumption.
+  - eexists. eexists. intuition. revert H2. simpl. unfold sOr. case RE.eqb_spec.
+    + unfold sCat. case RE.eqb_spec.
+      * simpl. intro. intro. intro. case RE.eqb_spec.
+        -- case RE.eqb_spec.
+          ++ intro. case RE.eqb_spec.
+            ** intro. case RE.eqb_spec.
+              --- intro. case RE.eqb_spec; rewrite e1 in e3; discriminate.
+              --- intro. case RE.eqb_spec.
+                +++ intro. case RE.eqb_spec; rewrite e2 in e3; discriminate.
+                +++ intro. case RE.eqb_spec.
+                  *** intro. simpl. intro. destruct e4, e0. induction x0.
+                    ---- simpl in H2. discriminate.
+                    ---- apply IHx0.
 Admitted.
 
 Lemma mixcat_gen r1 r2 l1 l2 :
